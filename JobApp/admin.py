f07d7fe99b725c4ac029eb08376bff7bab4b8050
UDIT VARSHNEY <41647477+UditVarshneyUV@users.noreply.github.com>
2021-02-26 17:17:46 +0530

    app file

---
+++ JobApp/admin.py
@@ -0,0 +1,6 @@
+from JobApp.models import RecruiterUser, StudentUser
+from django.contrib import admin
+
+# Register your models here.
+admin.site.register(StudentUser)
+admin.site.register(RecruiterUser)
